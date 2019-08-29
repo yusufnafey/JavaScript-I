d3817027499cebe57bf0700372edcde66a336df4
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-29 15:20:56 -0700

    finishes arrays challenge 2

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -91,8 +91,8 @@ console.log(
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 0;
-console.log();
+let lastCar = inventory[inventory.length - 1];
+console.log(`Make: ${lastCar.car_make}, Model: ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
